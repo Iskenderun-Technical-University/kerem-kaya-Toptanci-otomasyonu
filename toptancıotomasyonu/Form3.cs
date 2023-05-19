8dc95fb030b191a3b0395c9bcc152598808d096f
keremky <115721926+keremky@users.noreply.github.com>
2023-05-19 13:57:57 +0300

    kullanıcı ve şifre giriş işlemleri tanımlama yapıldı

---
+++ toptancıotomasyonu/Form3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace den
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String username = "dene";
+            String sifre = "123";
+            if(textBox1.Text== username && textBox2.Text== sifre)
+            {
+                Form8 form8 = new Form8();
+                form8.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adı veya Şifre yanlış.");
+            }
+        }
+    }
+}
