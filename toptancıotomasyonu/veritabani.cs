5f31f0dfe9a9bfaf5b99e0ff196db1800364cd47
keremky <115721926+keremky@users.noreply.github.com>
2023-05-29 19:21:53 +0300

    veritabanı bağlantısı kuruldu

---
+++ toptancıotomasyonu/veritabani.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace den
+{
+    class veritabani
+    {
+       static  SqlConnection con;
+       static SqlDataAdapter da;
+       static  SqlCommand cmd;
+       static DataSet ds;
+        public static String SqlCon = "Data Source=DESKTOP-TK2GC17\\SQLEXPRESS;Initial Catalog=toptanci;Integrated Security=True";
+
+        public static bool baglantiDurum()
+        {
+            using (con = new SqlConnection(SqlCon))
+                try
+                {
+                    con.Open();
+                    return true;
+                }
+                catch(SqlException ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                    return false;
+                }
+            
+        }
+
+
+    }
+}
